c1ab996f08483d3bf4bd15c4d3e3fd2b6f530fd1
Batxzy <jose.lopez4636@alumnos.udg.mx>
2024-11-20 14:42:16 -0600

    timer again

--- Prime_final/Prime_finalApp.swift
+++ Prime_final/Prime_finalApp.swift
@@ -5,34 +5,47 @@ struct Prime_finalApp: App {
     @State private var navPath = NavigationPath()
     @StateObject private var userManager = UserManager.shared
     @State private var showSplash = true
-    @State private var timeRemaining = 2
+    @State private var timeRemaining = 8
     @State private var timer: Timer?
 
     var body: some Scene {
         WindowGroup {
-            if showSplash {
-                PRIME()
-                    .onAppear {
-                        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
-                            if timeRemaining > 0 {
-                                timeRemaining -= 1
-                            } else {
-                                timer?.invalidate()
-                                showSplash = false
-                                
-                                // Handle navigation after splash
-                                if userManager.userDictionary.isEmpty {
-                                    navPath.append(AppRoute.createAccount)
-                                } else if userManager.currentUser == nil {
-                                    navPath.append(AppRoute.selectAccount)
-                                } else {
-                                    navPath.append(AppRoute.home)
+            ZStack {
+                if showSplash {
+                    PRIME()
+                        .onAppear {
+                            // Start timer only if it hasn't been started
+                            if timer == nil {
+                                timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+                                    withAnimation {
+                                        if timeRemaining > 0 {
+                                            timeRemaining -= 1
+                                        } else {
+                                            timer?.invalidate()
+                                            timer = nil
+                                            showSplash = false
+                                            
+                                            // Handle navigation after splash
+                                            if userManager.userDictionary.isEmpty {
+                                                navPath.append(AppRoute.createAccount)
+                                            } else if userManager.currentUser == nil {
+                                                navPath.append(AppRoute.selectAccount)
+                                            } else {
+                                                navPath.append(AppRoute.home)
+                                            }
+                                        }
+                                    }
                                 }
                             }
                         }
-                    }
-            } else {
-                RootView(Navpath: $navPath)
+                        .onDisappear {
+                            // Cleanup timer when view disappears
+                            timer?.invalidate()
+                            timer = nil
+                        }
+                } else {
+                    RootView(Navpath: $navPath)
+                }
             }
         }
     }
