d1c33e18b5bc11ea42d88d26d88be7238834d4cf
jose <joselooped@gmail.com>
2024-11-20 13:10:05 -0600

    prime prime primito

---
+++ Prime_final/navigation/PRIME.swift
@@ -0,0 +1,18 @@
+//
+//  PRIME.swift
+//  Prime_final
+//
+//  Created by Alumno on 20/11/24.
+//
+
+import SwiftUI
+
+struct PRIME: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PRIME()
+}
