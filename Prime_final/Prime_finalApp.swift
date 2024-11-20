3db5e96ad9d1773b733f5bbc6e4926785e2e1819
Batxzy <jose.lopez4636@alumnos.udg.mx>
2024-11-20 14:38:04 -0600

    timer

--- Prime_final/Prime_finalApp.swift
+++ Prime_final/Prime_finalApp.swift
@@ -1,29 +1,39 @@
 import SwiftUI
 
-
 @main
-
 struct Prime_finalApp: App {
     @State private var navPath = NavigationPath()
     @StateObject private var userManager = UserManager.shared
+    @State private var showSplash = true
+    @State private var timeRemaining = 2
+    @State private var timer: Timer?
 
     var body: some Scene {
         WindowGroup {
-            RootView(Navpath: $navPath)
-                .onAppear {
-                    // Reset navigation path first 
-                    navPath = NavigationPath()
-                    // Initial routing based on user state
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 15.0) { // 2 second delay
-                        if userManager.userDictionary.isEmpty {
-                            navPath.append(AppRoute.createAccount)
-                        } else if userManager.currentUser == nil {
-                            navPath.append(AppRoute.selectAccount)
-                        } else {
-                            navPath.append(AppRoute.home)
+            if showSplash {
+                PRIME()
+                    .onAppear {
+                        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+                            if timeRemaining > 0 {
+                                timeRemaining -= 1
+                            } else {
+                                timer?.invalidate()
+                                showSplash = false
+                                
+                                // Handle navigation after splash
+                                if userManager.userDictionary.isEmpty {
+                                    navPath.append(AppRoute.createAccount)
+                                } else if userManager.currentUser == nil {
+                                    navPath.append(AppRoute.selectAccount)
+                                } else {
+                                    navPath.append(AppRoute.home)
+                                }
+                            }
                         }
                     }
-                }
+            } else {
+                RootView(Navpath: $navPath)
+            }
         }
     }
 }
