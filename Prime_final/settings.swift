b7c4ece47b8c7509d1dfe4e68f9ef55c98c3053b
Batxzy <jose.lopez4636@alumnos.udg.mx>
2024-11-20 23:44:02 -0600

    ya quedo el view de settings

--- Prime_final/settings.swift
+++ Prime_final/settings.swift
@@ -10,41 +10,41 @@ struct Settings: View {
 
             VStack(spacing: 24){
                 
-                TextField("Enter your text here", text: $search)
+                TextField("Search by actor, title", text: $search)
                         .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .padding()
                         .background(Color.white)
-                .overlay(
-                    HStack {
-                        Image(systemName: "magnifyingglass")
-                            .foregroundColor(.gray)
-                            .padding(.leading, 8)
-                        Spacer()
-                    }
-                )
-                .padding(.leading, 24)
+                        .padding(.vertical, 10)
+                        .padding(.horizontal, 53)
+                    .overlay(
+                        HStack {
+                            Image(systemName: "magnifyingglass")
+                                .foregroundColor(.gray)
+                                .padding(.leading, 8)
+                            Spacer()
+                        }
+                    )
             //genres
-                VStack(alignment:.leading, spacing; 10){
-                    text(genres)
+                VStack(alignment:.leading, spacing: 10){
+                    Text("Genres")
                         .font(.system(size: 25, weight: .black))
                         .foregroundColor(.white)
-                        .frame(maxWidth: .infinity, maxHeight: 33 alignment: .leading)
+                        .frame(maxWidth: .infinity, maxHeight: 33, alignment: .leading)
                     
-                //generes chips
+            //generes chips
                         VStack(spacing: 13) {
                             VStack(spacing: 14){
-                                genre(name: "Action and adventure")
-                                genre(name: "Comedy")  
+                                GenreChip(genreName: "Action and adventure")
+                                GenreChip(genreName: "Anime")  
                             }
 
                             VStack(spacing: 14){
-                                genre(name: "Anime")
-                                genre(name: "Comedy")  
+                                GenreChip(genreName: "Comedy")
+                                GenreChip(genreName: "Documentary")  
                             }
 
                             VStack(spacing: 14){
-                                genre(name: "Drama")
-                                genre(name: "Fantasy")  
+                                GenreChip(genreName: "Drama")
+                                GenreChip(genreName: "Fantasy")  
                             }
                         }
                         .frame(maxWidth: .infinity)
@@ -52,30 +52,31 @@ struct Settings: View {
                     .frame(maxWidth: .infinity)
 
                     Capsule()
-                    .fill(Color.white.opacity(0.2))
-                    .frame(height: 40)
-                    .overlay(
-                        Text("See More")
-                            .font(.system(size: 15, weight: .bold))
-                            .foregroundColor(.white)
-                    )
+                        .fill(Color.white.opacity(0.2))
+                        .frame(height: 40)
+                        .overlay(
+                            Text("See More")
+                                .font(.system(size: 15, weight: .bold))
+                                .foregroundColor(.white)
+                        )
                 }
             }
                 .padding(.horizontal, 25)
                 .padding(.top, 125)
     }
+
 }
 
-struct Chipmovies: View {
-    var genre: String
+struct GenreChip: View {
+    var genreName: String
 
     var body: some View {
-        Text(genre)
+        Text(genreName)
             .font(.system(size: 15, weight: .bold))
             .foregroundColor(.white)
             .padding(13)
             .background(Color.white.opacity(0.2))
             .cornerRadius(8)
-            .frame(maxWidth: .infinity, maxWidth: .infinity)
+            .frame(maxWidth: .infinity)
     }
 }
